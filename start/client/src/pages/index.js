7fb0e61bd4c14444a63d3bf8a10ee4760c3632d8
lukevandekieft <vandekie@gmail.com>
2019-04-22 14:50:02 -0700

    Set up 1st half of page creation tutorials on client side.

--- start/client/src/pages/index.js
+++ start/client/src/pages/index.js
@@ -1,33 +1,11 @@
 import React, { Fragment } from 'react';
 import { Router } from '@reach/router';
-import { ApolloProvider } from 'react-apollo';
-import ReactDOM from 'react-dom';
-import Pages from './pages';
 
 import Launch from './launch';
 import Launches from './launches';
 import Cart from './cart';
 import Profile from './profile';
 import { Footer, PageContainer } from '../components';
-
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import gql from "graphql-tag";
-
-const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: 'http://localhost:4000/'
-})
-const client = new ApolloClient({
-  cache,
-  link
-})
-
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Pages />
-  </ApolloProvider>, document.getElementById('root'));
 
 export default function Pages() {
   return (
@@ -44,19 +22,3 @@ export default function Pages() {
     </Fragment>
   );
 }
-
-
-client
-  .query({
-    query: gql`
-      query GetLaunch {
-        launch(id: 56) {
-          id
-          mission {
-            name
-          }
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
