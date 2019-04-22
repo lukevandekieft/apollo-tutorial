434dd7ecf9a8053b9ffd51e8d8485b1eab0e5c10
lukevandekieft <vandekie@gmail.com>
2019-04-22 15:44:53 -0700

    Complete final setup of user functionality.

--- start/client/src/pages/launch.js
+++ start/client/src/pages/launch.js
@@ -9,6 +9,7 @@ import ActionButton from '../containers/action-button';
 export const GET_LAUNCH_DETAILS = gql`
   query LaunchDetails($launchId: ID!) {
     launch(id: $launchId) {
+      isInCart @client
       site
       rocket {
         type
